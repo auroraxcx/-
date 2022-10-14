6a41eef09da5696e4d30e45394c76856cb406ea1
auroraxcx <115385353+auroraxcx@users.noreply.github.com>
2022-10-14 11:54:26 +0800

    Add files via upload

--- xcx.c
+++ xcx.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 int main()
 {
-  int day = 0;
-  scanf("%d", &day);
-  if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5)
-    printf("休息日\n");
-  else if (day == 6 || day == 7)
-    printf("工作日\n");
-  else
-    printf("请输入数字1到7哦\n");
+  double a,b,max;
+  scanf("%lf%lf", &a,&b);
+  max=(a>b)?a:b;
+  printf("%lf",max);
   return 0;
 }
