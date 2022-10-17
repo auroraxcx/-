17f9070678def348e4734cddda8c1441e5d29512
auroraxcx <115385353+auroraxcx@users.noreply.github.com>
2022-10-17 19:21:53 +0800

    Add files via upload

--- helloworld.c
+++ helloworld.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 int main()
 {
-  int age;
-  scanf("%d",&age);
-  if (age<12&&age>0)
-  printf("年龄小于12\n");
-  else if(age>65&&age<150)
-  printf("年龄大于65\n");
-  else if(age<=0)
-  printf("别闹\n");
-  else if(age>=150)
-  printf("长寿\n");
-  else
-  printf("在12到65之间\n"); 
+    int i = 0;
+    do
+    {
+        printf("好累\n");
+        i++;
+        }
+        while (i <= 5);
+
     return 0;
 }
