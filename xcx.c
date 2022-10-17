2ad24e3b40a8b6922fd2e56f192f9437870e93e9
auroraxcx <115385353+auroraxcx@users.noreply.github.com>
2022-10-17 21:16:23 +0800

    Add files via upload

--- xcx.c
+++ xcx.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 int main()
 {
-int x,y;
-for (x=0,y=0;x<2&&y<5;++x,y++)
-{
-printf("xcx\n");
-}
+  int arr[] = {1, 2, 3, 6, 5, 6};
+  int k = 17;
+  int i = 0;
+  int sz = sizeof(arr) / sizeof(arr[0]);
+  for (i = 0; i < sz; i++)
+  {
+    if (k == arr[i])
+    {
+      printf("下标为%d \n", i);
+      continue;
+    }
+  }
+  if (i == sz)
+    printf("没有了\n");
   return 0;
 }
