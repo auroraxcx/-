acabf161bf7a4ff358f374ac6735ba464225d2a3
auroraxcx <115385353+auroraxcx@users.noreply.github.com>
2022-10-17 20:27:15 +0800

    Add files via upload

--- helloworld.c
+++ helloworld.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 int main()
 {
-    int i = 0;
-    do
+    int i, n, k;
+    printf("请输入一个整数哦\n");
+    scanf("%d", &i);
+    n = i;
+    if (i < 0)
+        printf("嘛呢哥们儿");
+    else if (i == 1 || i == 0)
+        printf("1");
+    else
     {
-        printf("好累\n");
-        i++;
-        }
-        while (i <= 5);
-
+        do
+        {
+            n = n * (i - 1);
+            i--;
+        } while (i > 1);
+        printf("%d", n);
+    }
     return 0;
 }
