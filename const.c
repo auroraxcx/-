0a99f5a50baf5167d6b02d029ca758c58e90d45c
auroraxcx <115385353+auroraxcx@users.noreply.github.com>
2022-12-09 11:01:19 +0800

    Add files via upload

--- const.c
+++ const.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
-#include <string.h>
 int main()
 {
-    int a[10];
-    int i, j, t;
-    printf("请输入十个数字\n");
-    for (i = 0; i < 10; i++)
-    {
-        scanf("%d", &a[i]);
-    }
-    for (j = 0; j < 9; j++)
-        for (i = 0; i < 9-j ; i++)
-            if (a[i] > a[i + 1])
-            {
-                t = a[i + 1];
-                a[i + 1] = a[i];
-                a[i] = t;
-            }
-    printf("结果是\n");
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d", a[i]);
-        printf("\n");
-    }
+    int f(int);
+    int a = 2, i;
+    for (i = 0; i < 3; i++)
+        printf("%d\n", f(a));
     return 0;
+}
+int f(int a)
+{
+    auto int b = 0;
+    static int c = 3;
+    b = b + 1;
+    c = c + 1;
+    return (a + b + c);
 }
