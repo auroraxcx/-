036d4434d88a3c2ccb9c423abbaf66df5e59da55
auroraxcx <115385353+auroraxcx@users.noreply.github.com>
2022-12-09 09:01:12 +0800

    Add files via upload

--- helloworld.c
+++ helloworld.c
@@ -1,35 +1,29 @@
-#include <stdio.h>
+#include <stdio.h> //6.写一个函数,将两个字符串连接。
+void link(char a[], char b[], char c[])
+{
+	int i, j = 0, k;
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		c[j] = a[i];
+		j++;
+	}
+	for (k = 0; b[k] != '\0'; k++)
+	{
+		c[j] = b[k];
+		j++;
+	}
+	printf("连接后字符串为：%s", c);
+}
 int main()
 {
-    int a[2][3], b[3][2];
-    int i, j;
-    for (i = 0; i < 2; i++)
-        for (j = 0; j < 3; j++)
-        {
-            scanf("%d", &a[i][j]);
-        }
-    printf("\n");
-    for (i = 0; i < 2; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            printf("%d\t", a[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 2; j++)
-        {
-            b[i][j] = a[j][i];
-        }
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 2; j++)
-        {
-            printf("%d\t", b[i][j]);
-        }
-        printf("\n");
-    }
-    return 0;
+	char str1[50], str2[50], str3[100];
+	printf("请输入第一个字符串：");
+	gets(str1);
+	printf("第一个字符串为：%s", str1);
+	printf("\n请输入第二个字符串:");
+	gets(str2);
+	printf("第二个字符串为：%s", str2);
+	getchar();
+	link(str1,str2,str3);
+	return 0;
 }
